f83bcc707812eccc03debba9329e5e1d4975d26e
Гриша <69295468+kra1dex@users.noreply.github.com>
2022-10-06 21:33:03 +0300

    когда бывшие видят меня они хором говорят чо

--- inquox_app/views.py
+++ inquox_app/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def to_login(request):
-  return render(request, 'inquox_app/login.html')
+  context = {
+    'title': 'Login'
+  }
+  return render(request, 'inquox_app/login.html', context=context)
 
 def to_register(request):
-  return render(request, 'inquox_app/register.html')
+  context = {
+    'title': 'Registration'
+  }
+  return render(request, 'inquox_app/register.html', context=context)
